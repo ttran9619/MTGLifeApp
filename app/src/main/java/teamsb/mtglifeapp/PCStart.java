6afa466049bed29c323d0ec9f05a08e5ef408b51
Tyler Tran <ttran@iastate.edu>
2015-04-10 23:13:35 -0500

    Added Login Start

--- app/src/main/java/teamsb/mtglifeapp/PCStart.java
+++ app/src/main/java/teamsb/mtglifeapp/PCStart.java
@@ -1,50 +1,24 @@
 package teamsb.mtglifeapp;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
-
-    Button LCbtn, PCbtn;
+public class PCStart extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        LCbtn = (Button) findViewById(R.id.LC);
-        PCbtn = (Button) findViewById(R.id.PC);
-
-        LCbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), LCLoginAct.class);
-                startActivity(intent);
-            }
-        });
-
-        PCbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), PCStart.class);
-                startActivity(intent);
-            }
-        });
-
-
+        setContentView(R.layout.activity_pcstart);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_pcstart, menu);
         return true;
     }
 
