33ae1f2ad9977a993c7408b3c3be991c6544308f
Tyler Tran <ttran@iastate.edu>
2015-04-11 23:17:35 -0500

    Basic

---
+++ app/src/main/java/teamsb/mtglifeapp/global.java
@@ -0,0 +1,10 @@
+package teamsb.mtglifeapp;
+
+/**
+ * Created by ttran_000 on 4/11/2015.
+ */
+public class global {
+    public static String hostName="BULL";
+    public static String guestName="SHIT";
+
+}
