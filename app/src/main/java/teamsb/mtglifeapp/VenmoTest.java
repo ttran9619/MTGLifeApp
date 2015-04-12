e2a940812fc63f8695686e231286a6e98a76971b
Tyler Tran <ttran@iastate.edu>
2015-04-11 22:25:33 -0500

    I love Android.....haha...haha..ha......ah

--- app/src/main/java/teamsb/mtglifeapp/VenmoTest.java
+++ app/src/main/java/teamsb/mtglifeapp/VenmoTest.java
@@ -111,7 +111,6 @@ public class VenmoTest extends ActionBarActivity {
             @Override
             public boolean onCreateOptionsMenu(Menu menu) {
                 // Inflate the menu; this adds items to the action bar if it is present.
-                getMenuInflater().inflate(R.menu.menu_venmo_test, menu);
                 return true;
             }
 
